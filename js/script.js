f7a9daf8f6642e2c2588909cab71b338d203918f
Yanira1202 <jmonge@estudiantes.crackthecode.la>
2022-01-19 10:14:41 -0500

    yanira

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const registroformulario= document.querySelector("#registro-formulario");
+const ingresoformulario= document.querySelector("#ingreso-formulario");
+
+//registro
+registroformulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = registroformulario["mail-new"].value;
+    const password = registroformulario["pass-new"].value;
+  console.log(email,password);
+    // Authenticate the User
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // clear the form
+        registroformulario.reset();
+        // close the modal
+        $("#registro").modal("hide");
+        console.log("registrado");
+      });
+  });
+  //Ingreso
+
+ingresoformulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = ingresoformulario["mail"].value;
+      const password = ingresoformulario["pass"].value;
+  
+    // Authenticate the User
+    auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+      // clear the form
+      ingresoformulario.reset();
+      console.log("logeado");
+    });
+  });
+  // evento: cambio de estado de autentificacion
+  const btnSalir =document.querySelector("#logout");
+auth.onAuthStateChanged((user) => {
+    if (user) {
+      console.log("logeado");
+      
+      btnSalir.style.display="block";
+     //formularioPost.style.display="block";
+    } else {
+      console.log("no has ingresado");
+     
+     //formularioPost.style.display="none";
+      btnSalir.style.display="none";
+    }
+  });
+
+  
