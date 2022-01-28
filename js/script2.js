eb8e61357dd8d872f25f9c73949490f6d5261731
Yanira1202 <jmonge@estudiantes.crackthecode.la>
2022-01-28 09:50:59 -0500

    JavaScript Finalizado

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,4 @@
-
-   const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAJ7-8Xka7KJC1td3S8847y62evPj-2LNA",
     authDomain: "proyecto-pancho-chancho-prueba.firebaseapp.com",
     projectId: "proyecto-pancho-chancho-prueba",
@@ -7,278 +6,82 @@
     messagingSenderId: "144688252808",
     appId: "1:144688252808:web:f2b71d17dec2d211685c3d",
     measurementId: "G-29VPCXCKR7"
-   };
-   // Initialize Firebase
-   firebase.initializeApp(firebaseConfig);
-    const auth = firebase.auth();
-    const post = firebase.firestore();
+};
 
-
-
-// Al enviar el Post con el boton Postear
-
-const  post_Form = document.getElementById("postForm")
-    
-
-post_Form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const mensaje = post_Form['postText'];
-  
-  await savePost(mensaje.value);
-  console.log("Post Guardado")
-  post_Form.reset();
-  mensaje.focus();
-
-});
-
-// Guardar post
-var date = new Date();
-const savePost = (mensaje) =>
-  post.collection("posts").add({
-    description: mensaje,
-    day: date.getDate(),
-    month: date.getMonth() + 1,
-    year: date.getFullYear(),
-    hour: date.getHours(),
-     minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
-
-  });
-
-  // Al cargar la Pagina, listar los post.
-  window.addEventListener('DOMContentLoaded', async (e) => {
-    const querySnapshot = await getPosts();
-
-    
-
-    querySnapshot.forEach(doc => {
-      
-      const PostList = doc.data();
-
-      PostList.innerHTML += `
-        
-        <div class="card card-body mt-2 border-primary">
-        <h5>${post.description}</h5>
-        <div>
-            <button class="btn btn-primary">Delete</button>
-            <button class="btn btn-secundary">Editar</button>
-        </div>
-        </div>
-      `
-    })
-
-  })
-// Obtener Post
-const getPosts = ()=> post.collection('posts').get();
-
-// Cunado se obtienen las tareas
-//const onGetPosts = (callback)=> post.collection('posts').onSnapshot(callback);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 //Inicializar servicio de la bd Firestore
-const db = firebase.firestore();
-//Publicar un nuevo estado
-$("#btnSendPost").click(function(e){
-    e.preventDefault();
-    let postText = $("#postText").val();
-    let date = new Date();
-    db.collection("posts").add({
-        description: postText,
-        day: date.getDate(),
-        month: date.getMonth() + 1,
-        year: date.getFullYear(),
-    })
-    .then((docRef)=>{
-        alert("Estado publicado");
-        $("#status-text").val('');
-        readPosts();
-        //readPosts2();
-    })
-    .catch((error) =>{
-        alert(error);
-    })
-})
 
-function readPosts(){
-    db.collection("posts").get().then((posts)=>{
+const db = firebase.firestore();
+
+//Selectores
+const post_Form = document.getElementById("postForm");
+const post_Form2 = document.getElementById("postForm2");
+
+/* METODOS DE FIREBASE STORE*/
+
+// Leer Estado
+function readPosts() {
+    db.collection('posts').get().then((posts) => {
         listPosts(posts.docs);
     })
 }
 
-function listPosts(data){
+// Leer Consejos
+function readPosts2() {
+    db.collection('posts2').get().then((posts2) => {
+        listPosts2(posts2.docs);
+    })
+}
+
+// Listar Estado
+function listPosts(data) {
     var divContent = $("#post-feed");
-    divContent.empty(); 
-    if(data.length > 0){
+    divContent.empty();
+    if (data.length > 0) {
         let content = "";
         data.forEach(document => {
             let doc = document.data();
             const divPost = `
-                <div style='border:solid 2px;'>
-                    <p>${doc.post}</p><br>
-                    <textarea style='display: none;'></textarea>
-                    <button data-id="${document.id}" style='display: none;'>Guardar</button>
-                    <span>Publicado el: ${doc.day}/${doc.month}/${doc.year}</span>
-                    <button data-id="${document.id}" class="btn btn-warning edit">Editar</button>
-                    <button data-id="${document.id}" class="btn btn-danger delete">Eliminar</button>
-                </div>
-                <hr>
-            `;
+                    <div style='border:solid 2px;'>
+                        <p>${doc.description}</p><br>
+                        <textarea style='display: none;'></textarea>
+                        <button data-id="${document.id}" style='display: none;'>Guardar</button>
+                        <span>Publicado el: ${doc.day}/${doc.month}/${doc.year}</span>
+                        <button data-id="${document.id}" class="btn btn-warning edit">Editar</button>
+                        <button data-id="${document.id}" class="btn btn-danger delete">Eliminar</button>
+                    </div>
+                    <hr>
+                `;
             content += divPost;
         });
         divContent.append(content);
         //Agregar listener a btn-delete
         const btnDelete = document.querySelectorAll(".delete");
-        btnDelete.forEach(btn=>{
-            btn.addEventListener("click",(e)=>{
+        btnDelete.forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                e.preventDefault();
                 const id = e.target.dataset.id;
                 DeletePost(id);
             })
         })
         const btnEdit = document.querySelectorAll(".edit");
-        btnEdit.forEach(btn=>{
-            btn.addEventListener("click",(e)=>{
+        btnEdit.forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                e.preventDefault();
                 const id = e.target.dataset.id;
-                OpenEdit(id,btn);
+                OpenEdit(id, btn);
             })
         })
     }
-}
+};
 
-function OpenEdit(id,button){
-    let parent = button.parentNode;
-    let textEdit = $(parent).children().eq(2);
-    let btnEdit = $(parent).children().eq(3);
-    textEdit.show();
-    btnEdit.show();
-    btnEdit.on("click",function(e){
-        SaveUpdate(e,id,textEdit.val())
-    });
-}
-
-function DeletePost(id){
-    db.collection("posts").doc(id).delete().then(() => {
-        alert("Se ha eliminado correctamente");
-        readPosts();
-       // readPosts2();
-    }).catch((error) => {
-        console.error("Detalle del Error: ", error);
-    });
-}
-
-
-function SaveUpdate(e,id_post,text_new){
-    e.preventDefault();
-    db.collection("posts").doc(id_post).update({
-        post: text_new,
-    }).then(()=>{
-        alert("Post actualizado");
-        readPosts();
-        //readPosts2();
-    })
-    .catch((error)=>{
-        alert("Error:",error);
-    })
-}
-
-
-
-
-
-
-
-
-// Al enviar el Post con el boton Postear
-
-const  post_Form2 = document.getElementById("postForm2")
-    
-
-post_Form2.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const mensaje = post_Form2['postText2'];
-  
-  await savePost(mensaje.value);
-  console.log("Post Guardado")
-  post_Form2.reset();
-  mensaje.focus();
-
-});
-
-// Guardar post
-var date = new Date();
-const savePost2 = (mensaje) =>
-  post.collection("posts2").add({
-    post: mensaje,
-    day: date.getDate(),
-    month: date.getMonth() + 1,
-    year: date.getFullYear(),
-    hour: date.getHours(),
-     minutes: date.getMinutes(),
-    seconds: date.getSeconds(),
-
-  });
-
-  // Al cargar la Pagina, listar los post.
-  window.addEventListener('DOMContentLoaded', async (e) => {
-    const querySnapshot = await getPosts2();
-
-    
-
-    querySnapshot.forEach(doc => {
-      
-      const PostList2 = doc.data();
-
-      PostList2.innerHTML += `
-        
-        <div class="card card-body mt-2 border-primary">
-        <h5>${post.post}</h5>
-        <div>
-            <button class="btn btn-primary">Delete</button>
-            <button class="btn btn-secundary">Editar</button>
-        </div>
-        </div>
-      `
-    })
-
-  })
-// Obtener Post
-const getPosts2 = ()=> post.collection('posts2').get();
-
-// Cunado se obtienen las tareas
-//const onGetPosts = (callback)=> post.collection('posts').onSnapshot(callback);
-//Inicializar servicio de la bd Firestore
-const db2 = firebase.firestore();
-//Publicar un nuevo estado
-$("#btnSendPost2").click(function(e){
-    e.preventDefault();
-    let postText2 = $("#postText2").val();
-    let date = new Date();
-    db2.collection("posts2").add({
-        post: postText2,
-        day: date.getDate(),
-        month: date.getMonth() + 1,
-        year: date.getFullYear(),
-    })
-    .then((docRef)=>{
-        alert("Estado publicado");
-        $("#status-text2").val('');
-        //readPosts();
-        readPosts2();
-    })
-    .catch((error) =>{
-        alert(error);
-    })
-})
-
-function readPosts2(){
-    db2.collection("posts2").get().then((posts2)=>{
-        listPosts2(posts2.docs);
-    })
-}
-
-function listPosts2(data){
+// Listar Consejos
+function listPosts2(data) {
     var divContent = $("#post-feed2");
-    divContent.empty(); 
-    if(data.length > 0){
+    divContent.empty();
+    if (data.length > 0) {
         let content = "";
         data.forEach(document => {
             let doc = document.data();
@@ -298,56 +101,177 @@ function listPosts2(data){
         divContent.append(content);
         //Agregar listener a btn-delete
         const btnDelete = document.querySelectorAll(".delete2");
-        btnDelete.forEach(btn=>{
-            btn.addEventListener("click",(e)=>{
+        btnDelete.forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                e.preventDefault();
                 const id = e.target.dataset.id;
-                DeletePost(id);
+                DeletePost2(id);
             })
         })
         const btnEdit = document.querySelectorAll(".edit2");
-        btnEdit.forEach(btn=>{
-            btn.addEventListener("click",(e)=>{
+        btnEdit.forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                e.preventDefault();
                 const id = e.target.dataset.id;
-                OpenEdit(id,btn);
+                OpenEdit2(id, btn);
             })
         })
     }
-}
+};
 
-function OpenEdit(id,button){
+// Abrir cuadros para Editar Estado
+function OpenEdit(id, button) {
     let parent = button.parentNode;
     let textEdit = $(parent).children().eq(2);
     let btnEdit = $(parent).children().eq(3);
     textEdit.show();
     btnEdit.show();
-    btnEdit.on("click",function(e){
-        SaveUpdate(e,id,textEdit.val())
+    btnEdit.on("click", function (e) {
+        SaveUpdate(e, id, textEdit.val())
     });
-}
+};
 
-function DeletePost2(id){
-    db2.collection("posts2").doc(id).delete2().then(() => {
-        alert("Se ha eliminado correctamente");
+// Abrir cuadros para Editar Consejo
+function OpenEdit2(id, button) {
+    let parent = button.parentNode;
+    let textEdit = $(parent).children().eq(2);
+    let btnEdit = $(parent).children().eq(3);
+    textEdit.show();
+    btnEdit.show();
+    btnEdit.on("click", function (e) {
+        SaveUpdate2(e, id, textEdit.val())
+    });
+};
+
+// Eliminar Estado
+function DeletePost(id) {
+    db.collection("posts").doc(id).delete().then(() => {
+        alert("El Estado se ha eliminado correctamente");
+        readPosts();
+        // readPosts2();
+    }).catch((error) => {
+        console.error("Detalle del Error: ", error);
+    });
+};
+
+//Eliminar Consejo
+function DeletePost2(id) {
+    db.collection("posts2").doc(id).delete().then(() => {
+        alert("El Consejo se ha eliminado correctamente");
         //readPosts();
         readPosts2();
     }).catch((error) => {
         console.error("Detalle del Error: ", error);
     });
+};
+
+// Actualizar Estado
+function SaveUpdate(e, id_post, text_new) {
+    e.preventDefault();
+    db.collection("posts").doc(id_post).update({
+        description: text_new,
+    }).then(() => {
+        alert("Post actualizado");
+        readPosts();
+    })
+        .catch((error) => {
+            alert("Error:", error);
+        })
 }
 
-function SaveUpdate(e,id_post,text_new){
+// Actualizar Consejo
+function SaveUpdate2(e, id_post, text_new) {
     e.preventDefault();
-    db2.collection("posts2").doc(id_post).update({
+    db.collection("posts2").doc(id_post).update({
         post: text_new,
-    }).then(()=>{
+    }).then(() => {
         alert("Post actualizado");
-        //readPosts();
         readPosts2();
     })
-    .catch((error)=>{
-        alert("Error:",error);
-    })
+        .catch((error) => {
+            alert("Error:", error);
+        })
 }
+
+/* EVENTOS*/
+// Al cargar la Pagina, listar los Estados.
+window.addEventListener('DOMContentLoaded', (e) => {
+    readPosts();
+});
+
+// Al cargar la Pagina, listar los Consejos.
+window.addEventListener('DOMContentLoaded', async (e) => {
+    readPosts2();
+});
+
+// Al Publicar un nuevo Estado
+$("#btnSendPost").click(function (e) {
+    e.preventDefault();
+    let postText = $("#postText").val();
+    let date = new Date();
+    db.collection("posts").add({
+        description: postText,
+        day: date.getDate(),
+        month: date.getMonth() + 1,
+        year: date.getFullYear(),
+    })
+        .then((docRef) => {
+            alert("Estado publicado");
+            $("#status-text").val('');
+            readPosts();
+        })
+        .catch((error) => {
+            alert(error);
+        })
+});
+
+// Al Publicar un nuevo Consejo
+$("#btnSendPost2").click(function (e) {
+    e.preventDefault();
+    let postText2 = $("#postText2").val();
+    let date = new Date();
+    db.collection("posts2").add({
+        post: postText2,
+        day: date.getDate(),
+        month: date.getMonth() + 1,
+        year: date.getFullYear(),
+    })
+        .then((docRef) => {
+            alert("Consejo publicado");
+            $("#status-text2").val('');
+            readPosts2();
+        })
+        .catch((error) => {
+            alert(error);
+        })
+});
+
+// Al Salir de la cuenta
+// Cambio de estado de autentificacion
+const btnSalir = document.querySelector("#logout");
+auth.onAuthStateChanged((user) => {
+    if (user) {
+      console.log("logeado");
+      
+      btnSalir.style.display="block";
+     //formularioPost.style.display="block";
+    } else {
+      console.log("no has ingresado");
+     
+     //formularioPost.style.display="none";
+     btnSalir.style.display="none";
+    }
+  });
+
+//Salir
+btnSalir.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("salistes de tu cuenta");
+    window.location.href = "./index.html"
+  });
+});
+
 
 
 
